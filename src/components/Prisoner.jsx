52d686d091473761fa61f6d97424aac651ff9f06
mubeess <mubarakibrahim2015@gmail.com>
2022-12-18 12:12:06 +0100

    done

--- src/components/Prisoner.jsx
+++ src/components/Prisoner.jsx
@@ -8,7 +8,7 @@ export default function Prisoner() {
         src='https://s.abcnews.com/images/US/william-garrison-ap-aa-200420_hpMain_16x9_992.jpg'
          alt='img'/>
         <div className='prisoner_detail'>
-            <h4>Panyau Yusuf</h4>
+            <h4>Umar Yusuf</h4>
             <span>5 years</span>
             <div className='priosner_icons'>
                 <DeleteOutlined style={{color:'red'}}/>
